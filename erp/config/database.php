f466227f22cf8a5c8cadd9c2a8a6cc2c5fb038e8
Nak <khat.ratanak@cloudnet.com.kh>
2018-03-27 16:34:55 +0700

    Nak upload system

--- erp/config/database.php
+++ erp/config/database.php
@@ -62,7 +62,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.1.16',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'iclouderp_v3_4_teatry',
